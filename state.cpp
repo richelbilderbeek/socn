311f49dd967cf4e540677a4568b031ab7a1ba266
richelbilderbeek <richel@richelbilderbeek.nl>
2020-04-23 15:00:13 +0200

    Count points

--- state.cpp
+++ state.cpp
@@ -1,14 +1,36 @@
 #include "state.h"
 #include <cassert>
 
-state::state()
+state::state(
+  const int n_villages,
+  const int n_cities,
+  const bool has_longest_road,
+  const bool has_biggest_knight_force,
+  const int n_development_points
+)
+  : m_has_biggest_knight_force{has_biggest_knight_force},
+    m_has_longest_road{has_longest_road},
+    m_n_cities{n_cities},
+    m_n_development_points{n_development_points},
+    m_n_villages{n_villages}
 {
 
 }
 
-bool has_won(const state& ) noexcept
+int count_points(const state& s) noexcept
 {
-  return false;
+  return
+      1 * s.get_n_villages()
+    + (2 * s.get_n_cities())
+    + (2 * s.get_has_longest_road())
+    + (2 * s.get_has_biggest_knight_force())
+    + s.get_n_development_points()
+  ;
+}
+
+bool has_won(const state& s) noexcept
+{
+  return count_points(s) >= 10;
 }
 
 void test_state()
@@ -16,5 +38,51 @@ void test_state()
   //A default state is not a win
   {
     assert(!has_won(state()));
+  }
+  //5 cities wins the game
+  {
+    state s(0, 5);
+    assert(s.get_n_villages() == 0);
+    assert(s.get_n_cities() == 5);
+    assert(!s.get_has_longest_road());
+    assert(!s.get_has_biggest_knight_force());
+    assert(has_won(s));
+  }
+  //2 villages and 4 cities wins the game
+  {
+    state s(2, 4);
+    assert(s.get_n_villages() == 2);
+    assert(s.get_n_cities() == 4);
+    assert(!s.get_has_longest_road());
+    assert(!s.get_has_biggest_knight_force());
+    assert(has_won(s));
+  }
+  //2 villages, 3 cities and longest road wins the game
+  {
+    state s(2, 3, true);
+    assert(s.get_n_villages() == 2);
+    assert(s.get_n_cities() == 3);
+    assert(s.get_has_longest_road());
+    assert(!s.get_has_biggest_knight_force());
+    assert(has_won(s));
+  }
+  //2 villages, 2 cities, longest road and biggest knight force wins the game
+  {
+    state s(2, 2, true, true);
+    assert(s.get_n_villages() == 2);
+    assert(s.get_n_cities() == 2);
+    assert(s.get_has_longest_road());
+    assert(s.get_has_biggest_knight_force());
+    assert(has_won(s));
+  }
+  //4 villages, 2 cities, 2 development points wins the game
+  {
+    state s(4, 2, false, false, 2);
+    assert(s.get_n_villages() == 4);
+    assert(s.get_n_cities() == 2);
+    assert(!s.get_has_longest_road());
+    assert(!s.get_has_biggest_knight_force());
+    assert(s.get_n_development_points() == 2);
+    assert(has_won(s));
   }
 }
--- state.h
+++ state.h
@@ -5,9 +5,40 @@
 class state
 {
 public:
-  state();
+  state(
+    const int n_villages = 2,
+    const int n_cities = 0,
+    const bool has_longest_road = false,
+    const bool has_biggest_knight_force = false,
+    const int n_development_points = 0
+  );
+
+  bool get_has_biggest_knight_force() const noexcept { return m_has_biggest_knight_force; }
+  bool get_has_longest_road() const noexcept { return m_has_longest_road; }
+  int get_n_cities() const noexcept { return m_n_cities; }
+  int get_n_development_points() const noexcept { return m_n_development_points; }
+  int get_n_villages() const noexcept { return m_n_villages; }
+
+private:
+
+  /// Does the player have the biggest knight force?
+  bool m_has_biggest_knight_force;
+
+  /// Does the player have the longest road?
+  bool m_has_longest_road;
+
+  /// Number of cities
+  int m_n_cities;
+
+  /// Number of development points, from cards
+  int m_n_development_points;
+
+  /// Number of villages
+  int m_n_villages;
 };
 
+/// Count the number of points
+int count_points(const state& s) noexcept;
 
 /// Is the state winning?
 bool has_won(const state& s) noexcept;
